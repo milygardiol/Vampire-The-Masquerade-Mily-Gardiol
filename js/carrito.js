98d2a6310ae0343797f036e777762053fc3622bd
Milagros Gardiol <gardiol72@gmail.com>
2022-07-09 21:50:02 -0300

    Empleando APIs para un mejor funcionamiento

--- js/carrito.js
+++ js/carrito.js
@@ -1,133 +1,33 @@
 // VARIABLES
 document.addEventListener('DOMContentLoaded', () => { 
-    const librosRol = [
-        {
-            id : 1,
-            nombre: "Vampiro La Mascarada",
-            idioma: "Español",
-            edicion: "5ta Edición",
-            precio: 200
-        },
-        {
-            id: 2,
-            nombre: "Camarilla" ,
-            idioma: "Español",
-            edicion: "5ta Edición",
-            precio: 170
-        },
-        {
-            id: 3,
-            nombre: "Anarquistas",
-            idioma: "Inglés",
-            edicion: "5ta Edición",
-            precio: 160
-        },
-        {
-            id: 4,
-            nombre: "Chicago By Night",
-            idioma: "Inglés",
-            edicion: "5ta Edición",
-            precio: 160
-        },
-        {
-            id: 5,
-            nombre: "Chicago Folios",
-            idioma: "Inglés",
-            edicion: "5ta Edición",
-            precio: 150
-        },
-        {
-            id: 6,
-            nombre:  "Let the Streets Run Red",
-            idioma: "Inglés",
-            edicion: "5ta Edición",
-            precio: 158
-        },
-        {
-            id: 7,
-            nombre:  "La Caída de Londres",
-            idioma: "Español",
-            edicion: "5ta Edición",
-            precio: 180
-        },
-        {
-            id: 8,
-            nombre:  "Companion",
-            idioma: "Inglés",
-            edicion: "5ta Edición",
-            precio: 150
-        },
-        {
-            id: 9,
-            nombre:  "Cults of the Blood Gods",
-            idioma: "Inglés",
-            edicion: "5ta Edición",
-            precio: 190
-        },
-        {
-            id: 10,
-            nombre:  "Trails of Ash and Bone",
-            idioma: "Inglés",
-            edicion: "5ta Edición",
-            precio: 160
-        },
-        {
-            id: 11,
-            nombre:  "Children of the Blood",
-            idioma: "Inglés",
-            edicion: "5ta Edición",
-            precio: 160
-        },
-        {
-            id: 12,
-            nombre:  "Forbidden Religions",
-            idioma: "Inglés",
-            edicion: "5ta Edición",
-            precio: 160
-        },
-        {
-            id: 13,
-            nombre:  "Sabbat La Mano Negra",
-            idioma: "Español",
-            edicion: "5ta Edición",
-            precio: 190
-        },
-        {
-            id: 14,
-            nombre:  "Segunda Inquisición",
-            idioma: "Español",
-            edicion: "5ta Edición",
-            precio: 190
-        }
-    ];
 
     let carrito = [];
-            const divisa = '$';
-            const items = document.querySelector('#items');
-            const miCarrito = document.querySelector('#carrito');
-            const total = document.querySelector('#total');
-            const botonVaciar = document.querySelector('#boton-vaciar');
-            const botonComprar = document.querySelector("#botonComprar");
-            const localStorage = window.localStorage;
+    const divisa = '$';
+    const items = document.querySelector('#items');
+    const miCarrito = document.querySelector('#carrito');
+    const total = document.querySelector('#total');
+    const botonVaciar = document.querySelector('#boton-vaciar');
+    const botonComprar = document.querySelectorAll("#boton-comprar")
+    const localStorage = window.localStorage;
 
             // Funciones
 
-            function renderizarProductos() {
+    function renderizarProductos() {
                 librosRol.forEach((info) => {
                     // Boton 
-                    botonComprar.classList.add('btn', 'btn-primary');
+                    botonComprar.classList.add("btn", "btn-primary");
                     botonComprar.setAttribute('marcador', info.id);
                     botonComprar.addEventListener('click', comprarCarrito);
                 });
-            }
+    }
 
-            function comprarCarrito(evento) {
+    function comprarCarrito(evento) {
                 carrito.push(evento.target.getAttribute('marcador'))
                 renderizarCarrito();
                 guardarCarritoEnLocalStorage();
-            }
+    }
 
-            function renderizarCarrito() {
+    function renderizarCarrito() {
                 miCarrito.textContent = '';
                 const carritoSinDuplicados = [...new Set(carrito)];
                 carritoSinDuplicados.forEach((item) => {
@@ -157,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 total.textContent = calcularTotal();
             }
 
-            function borrarItemCarrito(evento) {
+    function borrarItemCarrito(evento) {
                 const id = evento.target.dataset.item;
                 carrito = carrito.filter((carritoId) => {
                     return carritoId !== id;
@@ -167,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             }
 
-            function calcularTotal() {
+    function calcularTotal() {
                 return carrito.reduce((total, item) => {
                     const miItem = librosRol.filter((itemlibrosRol) => {
                         return itemlibrosRol.id === parseInt(item);
@@ -176,17 +76,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 }, 0).toFixed(2);
             }
 
-            function vaciarCarrito() {
+    function vaciarCarrito() {
                 carrito = [];
                 renderizarCarrito();
                 localStorage.clear();
             }
 
-            function guardarCarritoEnLocalStorage () {
+    function guardarCarritoEnLocalStorage () {
                 localStorage.setItem('carrito', JSON.stringify(carrito));
             }
 
-            function cargarCarritoDeLocalStorage () {
+    function cargarCarritoDeLocalStorage () {
                 if (localStorage.getItem('carrito') !== null) {
                     carrito = JSON.parse(localStorage.getItem('carrito'));
                 }
--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,5 +1,122 @@
 /* INICIO DEL PROYECTO FINAL */
 
+window.addEventListener("DOMContentLoaded", ()=> {
+    fetch("http://localhost:3000/librosRol")
+    .then (response => response.json())
+    .then (librosRol => {
+        // Función asincrónica
+        renderizarLibros(librosRol);
+
+    })
+    .catch(er=> console.log(er))
+    .finally[() => console.log ("final")]
+})
+
+function renderizarLibros(librosRol) {
+    // Utilizo la forma de desestructurar el .forEach() para practicar este método
+    librosRol.forEach(({img, nombre, descripcion, edicion, idioma, precio}) => {   
+        const librosHTML = `
+        <ul>
+            <li>
+              <div class="card" style="width: 18rem;">
+                <img src=" ${img} " class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">${nombre}</h5>
+                  <p class="card-text">${descripcion}</p>
+                  <p class="edition">${edicion}</p>
+                  <p class="card-idiom"> ${idioma}</p>
+                  <span>$${precio} USD</span>
+                  <button class="btn btn-primary" id="boton-comprar">Buy it</button>
+                </div>
+              </div>
+            </li>
+        </ul>
+        `;
+        document.getElementById("books").innerHTML += librosHTML;
+    })
+
+    // Establezco el filtrado de idiomas de los libros
+    const tipoIdioma = ["Spanish", "English", "All"]
+
+
+    tipoIdioma.forEach(idioma => {
+        // Creo los botones para los idiomas
+        const btnTipo = document.createElement("button");
+        btnTipo.innerHTML = idioma;
+        btnTipo.classList.add("btn", "btn-primary", "m-2");
+
+        // Botón para "All"
+        btnTipo.addEventListener("click", () => {
+            // Opción "All"
+            if (idioma === "All"){
+                // Vacío el HTML antes de reponer las opciones en la página
+                document.getElementById("books").innerHTML="";
+                // Vuelvo a invocar la rest API para que me devuelva los libros cargados
+                fetch("http://localhost:3000/librosRol")
+                .then((response) => response.json())
+                .then(librosRol => {
+
+                    librosRol.forEach(librosRol => {
+
+                        const librosHTML = `
+                                <li>
+                                    <div class="card" style="width: 18rem;">
+                                        <img src=" ${librosRol.img} " class="card-img-top">
+                                        <div class="card-body">
+                                        <h5 class="card-title">${librosRol.nombre}</h5>
+                                        <p class="card-text">${librosRol.descripcion}</p>
+                                        <p class="edition">${librosRol.edicion}</p>
+                                        <p class="card-idiom"> ${librosRol.idioma}</p>
+                                        <span>$${librosRol.precio} USD</span>
+                                        <button class="btn btn-primary" id="boton-comprar">Buy it</button>
+                                        </div>
+                                    </div>
+                                </li>
+                            `;
+                            document.getElementById("books").innerHTML += librosHTML;
+
+                    })
+
+                .catch((err) => console.error);
+
+                })
+                // Continúo el if, para los otros botones
+                // Empleo el método .filter() para que me devuelva los datos de mi array según el idioma 
+            } else {
+                const librosFiltrados = librosRol.filter(lib => lib.idioma === idioma);
+                console.log(librosFiltrados);
+                document.getElementById("books").innerHTML="";
+
+                // Recorro los libros para filtrar por idioma seleccionado y devolver las cards
+                librosFiltrados.forEach(librosRol => {
+
+                    const librosHTML = `
+                            <li>
+                                <div class="card" style="width: 18rem;">
+                                    <img src=" ${librosRol.img} " class="card-img-top">
+                                    <div class="card-body">
+                                    <h5 class="card-title">${librosRol.nombre}</h5>
+                                    <p class="card-text">${librosRol.descripcion}</p>
+                                    <p class="edition">${librosRol.edicion}</p>
+                                    <p class="card-idiom"> ${librosRol.idioma}</p>
+                                    <span>$${librosRol.precio} USD</span>
+                                    <button class="btn btn-primary" id="boton-comprar">Buy it</button>
+                                    </div>
+                                </div>
+                            </li>
+                        `;
+                        document.getElementById("books").innerHTML += librosHTML;
+                    })
+                }
+            })
+            // Coloco los botones de los idiomas debajo de el div class= "tipos"
+        document.getElementById("tipos").appendChild(btnTipo);
+    })
+}
+
+
+// Funciones para el formulario de inicio
+// Guardar los datos del usuario en localStorage
 function guardarDatosDeUsr() {
     const datosDeUsr = {nombre: inputNombre.value,
                         edad: inputEdad.value,
@@ -12,9 +129,9 @@ function guardarDatosDeUsr() {
     aviso.textContent = "The form was sent!"
 }
 
-submit.addEventListener("click", guardarDatosDeUsr)
+submit.addEventListener("click", guardarDatosDeUsr);
 
-
+// Devolver los datos del usuario registrado como objeto
 function recuperoDatosDeUsr() {
         if (localStorage.getItem("datosDeUsr")) {
             const datosDeUsr = JSON.parse(localStorage.getItem("datosDeUsr"))
@@ -24,6 +141,6 @@ function recuperoDatosDeUsr() {
         }    
     }
 
-recuperoDatosDeUsr()
+recuperoDatosDeUsr();
 
 
